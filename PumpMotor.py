df2fcb9de4bf95ef4e2084db235f854a6c9560d0
Mboizi <mboizi@localhost.localdomain>
2022-08-22 13:17:02 +0300

    water pump

---
+++ PumpMotor.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+channel = 21 # pin 40
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM) # sets numbering system to be used. Options are: BCM, BOARD
+GPIO.setup(channel, GPIO.OUT, initial=GPIO.HIGH)
+
+def motor_on(pin):
+    GPIO.output(pin, GPIO.LOW)  # Turn motor on 
+
+def motor_off(pin):
+    GPIO.output(pin, GPIO.HIGH)  # Turn motor off
+
+while True:
+    try:
+        motor_on(channel)
+        print("ON")
+        time.sleep(5)
+    
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
